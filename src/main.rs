4398874707ef57dfead1efdd10afb7e396e3fa46
Joschka Thurner <git@joschkathurner.de>
2023-04-12 23:07:20 +0200

    Read allowed IDs from config file

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,8 @@ use getraenkekassengeraete::nfcservice;
 use hyperlocal::UnixServerExt;
 use keyturner::Keyturner;
 use pairing::{AuthInfo, PairingClient};
-use tokio::sync::Mutex;
+use serde::Deserialize;
+use tokio::{fs::read_to_string, sync::Mutex};
 
 use crate::client::UnconnectedClient;
 
@@ -27,10 +28,18 @@ mod pairing;
 const LOCK_ADDRESS: [u8; 6] = [0x54, 0xD2, 0x72, 0xAC, 0x8D, 0xC5];
 const APP_ID: u32 = 0x4d9edba9;
 const APP_NAME: &str = "KalkSpace";
-const ALLOWED_CARD_IDS: &[&str] = &["01856030-2324-47e3-ac1e-d3cb0ddabbef"];
+
+#[derive(Deserialize, Debug)]
+struct Config {
+    phone_ids: Vec<String>,
+    card_ids: Vec<[u8; 4]>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
+    let config = read_to_string("config.json").await?;
+    let config: Config = serde_json::from_str(&config)?;
+
     sodiumoxide::init().map_err(|_| anyhow!("Failed to initialize sodiumoxide..."))?;
 
     let stream = nfcservice::run().unwrap();
@@ -59,7 +68,7 @@ async fn main() -> Result<(), anyhow::Error> {
 
         match item {
             nfcservice::CardDetail::MeteUuid(uuid) => {
-                if ALLOWED_CARD_IDS.contains(&&*uuid) {
+                if config.phone_ids.contains(&uuid) {
                     keyturner.run_action(LockAction::Unlock).await?;
                 }
             }
